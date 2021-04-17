fd7e796469b451ac07e2b21910f932ba7d54b159
prabal <sprabal01@gmail.com>
2021-04-17 22:07:55 +0530

    login page gui done

--- src/Components/login_page/login_component.js
+++ src/Components/login_page/login_component.js
@@ -4,8 +4,11 @@ export function DivCard() {
     return (
         <div className="divCard">
             <div className="formDiv">
-                <input type="text" />
-                <input type="password" />
+                <h1>Login Page</h1>
+                <div className="inputFields">
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                </div>
                 <button> Login </button>
             </div>
         </div>
