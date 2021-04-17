42fe9f96c16660068301cc55642326cca115a5ef
prabal <sprabal01@gmail.com>
2021-04-17 20:32:02 +0530

    aklshdfl

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import LoginPage from './Components/login_page';
+import { DivCard } from './Components/login_page/login_component';
 
 function App() {
   return (
     <div className="App">
-      
+      <LoginPage/>
     </div>
   );
 }
---
+++ src/Components/login_page/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { DivCard } from './login_component'
+import "./stylesheet/style.css"
+export default function LoginPage() {
+    return (
+        <div>
+            <DivCard/>
+        </div>
+    )
+}
